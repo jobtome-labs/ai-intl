1b4d4d1c0b1ab30da76fa603c6651f2d403123ce
Erik <erik@Eriks-MacBook-Pro.local>
2023-02-24 14:07:42 +0100

    Test pre-commit hook

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,38 @@
-console.log("Ai-Intl");
+import { cli } from "cleye";
+import { description, version } from "../package.json";
+import hookCommand, { isCalledFromGitHook } from "./commands/hook.js";
+import { translate } from "./commands/translate.js";
+
+const rawArgv = process.argv.slice(2);
+
+const response = cli(
+  {
+    name: "ai-intl",
+    version,
+    description,
+    flags: {
+      apiKey: {
+        type: String,
+        description: "OpenAI API Key",
+      },
+    },
+
+    commands: [hookCommand],
+
+    help: {
+      description,
+    },
+  },
+  (argv) => {
+    if (isCalledFromGitHook) {
+      console.log("This is called from github hook");
+    } else {
+      // translate({
+      //   apiKey: argv.flags.apiKey,
+      // });
+    }
+  },
+  rawArgv
+);
+
+console.log(response);
